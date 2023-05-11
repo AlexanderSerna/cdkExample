6d0c013599edfef7355917753868cfd017320ab4
LAPTOP-15ON5BO2\Alexander Serna <alexander.agudelo@itconsultores.com.co>
2023-05-11 09:05:49 -0500

    Initial commit

---
+++ test/lambda-example.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaExample from '../lib/lambda-example-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-example-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaExample.LambdaExampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
